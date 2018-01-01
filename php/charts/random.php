c3a5a928983767d40ab518425fe75cc7dceb6b96
simmzl <czl008@outlook.com>
2018-01-01 23:44:56 +0800

    修复后端数据库结构更改之后不生成模拟数据的问题

--- php/charts/random.php
+++ php/charts/random.php
@@ -2,37 +2,36 @@
 
 header('Content-Type:text/plain;charset=utf-8');
 
-$a=rand(0,30);
-$b=rand(0,70);
-$c=rand(0,10);
-$x=rand(0,10);
-$z=2;
+// $table_name = '00000001';
+function creat_random($table_name){
+  $a=rand(0,30);
+  $b=rand(0,70);
+  $c=rand(0,10);
+  $x=rand(0,10);
+  $z=2;
 
-$id=1;
-$date=time();
-$ws=($c/10);
-$temp=$a+($x/10);
-$hum=$b;
+  $date=time();
+  $ws=($c/10);
+  $temp=$a+($x/10);
+  $hum=$b;
 
-$con = mysql_connect("XXX","XXX","XXX");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
+  $con = mysql_connect("xx","ss","ss");
+  if (!$con){
+    die('Could not connect:' . mysql_error());
   }
 
-// mysql_query("set names utf-8");
+  mysql_select_db("ss", $con);
 
+  $sql = "INSERT INTO `{$table_name}` 
+  (`id`,`date`,`wind_speed`,`temperature`,`humidity`)
+  VALUES
+  (NULL,'$date','$ws','$temp','$hum')";
+  mysql_query($sql,$con);
+  mysql_close($con);
+}
 
-// Create table in my_db database
+creat_random("00000001");
+creat_random("00000002");
+creat_random("00000003");
 
-mysql_select_db("XXX", $con);
-
-	$sql = "INSERT INTO `xx` 
-	(`xx`,`xx`,`xx`,`xx`,`xx`,`xx`)
-	VALUES
-	(NULL,'$date','$ws','$temp','$hum','$z')";
-	mysql_query($sql,$con);
-
-
-	mysql_close($con);
 ?>
